85878e5919ea0bf913fe83bdbf4b8f83ad07379b
vishal <hi.vishal123@gmail.com>
2015-04-21 19:16:58 +0530

    resolved a conflit between czmlwriter.JulianDate and agi.JulianDate

--- src/gov/nasa/worldwindx/examples/WedgeWater/CreateWedge.java
+++ src/gov/nasa/worldwindx/examples/WedgeWater/CreateWedge.java
@@ -1,12 +1,27 @@
 package gov.nasa.worldwindx.examples.WedgeWater;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
+import org.joda.time.DateTime;
+
+import agi.foundation.time.GregorianDate;
+//import agi.foundation.time.JulianDate;
+//import agi.foundation.time.TimeInterval;
+import agi.foundation.time.TimeStandard;
 import cesiumlanguagewriter.Cartographic;
+import cesiumlanguagewriter.CesiumIntervalListWriter;
 import cesiumlanguagewriter.CesiumOutputStream;
 import cesiumlanguagewriter.CesiumStreamWriter;
+import cesiumlanguagewriter.DoubleCesiumWriter;
+import cesiumlanguagewriter.JulianDate;
 import cesiumlanguagewriter.PacketCesiumWriter;
+import cesiumlanguagewriter.PolygonCesiumWriter;
 import cesiumlanguagewriter.SolidColorMaterialCesiumWriter;
+import cesiumlanguagewriter.TimeInterval;
+import cesiumlanguagewriter.advanced.CesiumPropertyWriter;
 
 public class CreateWedge {
 	public void create(CesiumOutputStream output){
@@ -77,15 +92,24 @@ public class CreateWedge {
 
 		listofPositions.add(arr1);
 
+		Calendar startDate = new GregorianCalendar(TimeZone.getTimeZone("GMT"));
+		startDate.set(2012, 8, 4, 0, 0, 0);
+		Calendar endDate = Calendar.getInstance();
+		endDate.add(Calendar.DATE, 2);
+
 		for(int i=0 ; i<listofPositions.size() ; i++){
 			PacketCesiumWriter packet = stream.openPacket(output);
 			packet.writeId(namesId[i]);
 			cesiumlanguagewriter.PolygonCesiumWriter polygon = packet.openPolygonProperty();
 			
 			//List<cesiumlanguagewriter.CartographicDegrees> positions = new ArrayList<cesiumlanguagewriter.CartographicDegrees>();
-
 			
 			polygon.writePositionsPropertyCartographicDegrees(listofPositions.get(i));
+
+			JulianDate startJD = new JulianDate(new DateTime(startDate.getTime()));
+			JulianDate endJD = new JulianDate(new DateTime(endDate.getTime()));
+			
+			packet.writeAvailability(startJD, endJD);
 			//polygon.writePositionsPropertyReferences(references);
 			//Material Property
 			cesiumlanguagewriter.MaterialCesiumWriter materialWriter = polygon.openMaterialProperty();
